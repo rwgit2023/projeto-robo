9fba4c4177ed549938c33c6125731397e41601d6
rwgit2023 <ronangit2023@hotmail.com>
2023-02-22 16:16:04 -0300

    Sixth Commit

--- js/main.js
+++ js/main.js
@@ -15,19 +15,15 @@
 
 // const subtrair = document.querySelector('#subtrair')
 // const somar = document.querySelector('#somar')
-const braco = document.querySelector('#braco')
 
-const controle = document.querySelectorAll('.controle-ajuste')
+const controle = document.querySelectorAll('[data-controle]')
 
  controle.forEach(function(elemento) {             //usamos o foreach para buscar dentro do array do controle os botões existentes e assim retornando o elemento dentro dele do btao (+ ou -)
     elemento.addEventListener("click", function(evento){//  Escutador de click do evento
-        manipulaDados(evento.target.textContent)
-    
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode) // Estou enviando 2 parâmetros para a função 1 - O sinal de + ou -   2 - Qual o pai dos controles (display que sera alterado)  
     } )
     
 })
-
-
 
 
 // somar.addEventListener("click", function(){
@@ -40,11 +36,14 @@ const controle = document.querySelectorAll('.controle-ajuste')
 
 
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+
+    const peca = controle.querySelector('[data-contador]')
+
     if(operacao === "-") {
-        braco.value = parseInt(braco.value) - 1
+        peca.value = parseInt(peca.value) - 1
     } else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
 }
 
