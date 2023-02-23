cfa7aa47ca1272d8e56c675020d0414e84c45109
rwgit2023 <ronangit2023@hotmail.com>
2023-02-23 14:43:06 -0300

    Eight Commit

--- js/main.js
+++ js/main.js
@@ -5,11 +5,48 @@
 // Toda vez que for input é um value     
 // Toda vez que for um texto é um textContent
 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 const controle = document.querySelectorAll('[data-controle]')
+const estatisticas = document.querySelectorAll('[data-estatistica]')
+
 
  controle.forEach(function(elemento) {             //usamos o foreach para buscar dentro do array do controle os botões existentes e assim retornando o elemento dentro dele do btao (+ ou -)
     elemento.addEventListener("click", function(evento){//  Escutador de click do evento
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode) // Estou enviando 2 parâmetros para a função 1 - O sinal de + ou -   2 - Qual o pai dos controles (display que sera alterado)  
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode) // Estou enviando 2 parâmetros para a função 1 - O sinal de + ou -   2 - Qual o pai dos controles (display que sera alterado)
+        atualizaEstatisticas (evento.target.dataset.peca) 
     } )
     
 })
@@ -25,4 +62,10 @@ function manipulaDados(operacao, controle) {
     }
 }
 
+function atualizaEstatisticas(peca){
+    estatisticas.forEach(function(elemento){
 
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca] [elemento.dataset.estatistica]
+
+    })
+}
