8dbfea51b4e68f5b61d9cb9e2489b19b5dbb799d
rwgit2023 <ronangit2023@hotmail.com>
2023-02-23 10:52:47 -0300

    Seventh Commit

--- js/main.js
+++ js/main.js
@@ -2,19 +2,8 @@
 // As funções nomeadas podem ser reutilazadas em diversos momentos
 // As funções anônimas servem quando o código será utilizado apenas 1x
 
-
-// const robotron = document.querySelector('.robo')
-
-// robotron.addEventListener("click", function(evento){
-//     console.log(evento)
-// })
-
-// function dizOi(nome){
-//     alert(nome + ' Bem vindo ao Robotron')  
-// }
-
-// const subtrair = document.querySelector('#subtrair')
-// const somar = document.querySelector('#somar')
+// Toda vez que for input é um value     
+// Toda vez que for um texto é um textContent
 
 const controle = document.querySelectorAll('[data-controle]')
 
@@ -24,17 +13,6 @@ const controle = document.querySelectorAll('[data-controle]')
     } )
     
 })
-
-
-// somar.addEventListener("click", function(){
-//     manipulaDados("somar")
-// })
-
-// subtrair.addEventListener("click", function(){
-//     manipulaDados("subtrair")
-// })
-
-
 
 function manipulaDados(operacao, controle) {
 
